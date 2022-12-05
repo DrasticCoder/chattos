250eca1c2843258da11e4b65ecd30b6409f38f3b
DrasticCoder <drasticoder@gmail.com>
2022-12-05 18:02:59 +0530

    feat: add join chat functionality

--- public/client.js
+++ public/client.js
@@ -5,23 +5,26 @@ const msgSetAudio = new Audio(`/msgSendAudio.mp3`);
 const msgGetAudio = new Audio('/msgGetAudio.mp3');
 
 let name = localStorage.getItem('name');
-let textarea = document.querySelector('#textarea')
-let messageArea = document.querySelector('.message__area')
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
+let optionsMenu = document.getElementById('options__menu');
+let joinSection = document.getElementById('join__section');
+let joinChatId = document.getElementById('join_chat_id');
 
 while (!name) {
     name = prompt('Please enter your name: ')
     localStorage.setItem('name',name)
 }
 
-textarea.addEventListener('keyup', (e) => {
-    if(e.key === 'Enter') {
-        sendMessage(e.target.value)
-        msgSetAudio.play();
-    }
-})
+// textarea.addEventListener('keyup', (e) => {
+//     if(e.key === 'Enter') {
+//         sendMessage(e.target.value)
+//         msgSetAudio.play();
+//     }
+// })
 
 function sendMessageBtn(){
-    const msgContent = document.getElementById('textarea').value;
+    const msgContent = textarea.value;
     sendMessage(msgContent);
     msgSetAudio.play();
 }
@@ -67,4 +70,26 @@ function scrollToBottom() {
 }
 
 
+// utils
 
+function openOptionsMenu(e){
+    if(optionsMenu.classList.contains('hidden')){
+        optionsMenu.classList.remove('hidden');
+    } else{
+        optionsMenu.classList.add('hidden');
+    }
+}
+
+function openJoinModal(){
+    if(joinSection.classList.contains('hidden')){
+        joinSection.classList.remove('hidden');
+    } else{
+        joinSection.classList.add('hidden');
+    }
+}
+
+// options funcs
+
+function joinChat(){
+    window.location.replace(`${window.location.origin}/${joinChatId.value}`)
+}
